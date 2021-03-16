190f43867acaa2f71bb9fdcfbcf9948ddfb036dc
ToruNiina <niina.toru.68u@gmail.com>
2021-03-16 12:10:10 +0900

    :recycle: update main

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,9 @@
 
 int main()
 {
-    gf11::GreensFunction3DAbsSym<double>     gf_11(1.0, 1.0);
-    greens_functions::GreensFunction3DAbsSym gf_98(1.0, 1.0);
+    namespace gf98 = greens_functions;
+    gf11::GreensFunction3DAbsSym gf_11(1.0, 1.0);
+    gf98::GreensFunction3DAbsSym gf_98(1.0, 1.0);
     std::cout << "# rnd   gf11   gf98    abserr (gf11 - gf98)   relerr (|gf11/gf98 - 1|)\n";
     for(std::size_t i=0; i<10000; ++i)
     {
@@ -15,5 +16,6 @@ int main()
         std::cout << rnd << ' ' << t11 << ' ' << t98 << ' '
                   << t11 - t98 << ' ' << std::abs(t11 / t98 - 1.0) << '\n';
     }
+
     return 0;
 }
