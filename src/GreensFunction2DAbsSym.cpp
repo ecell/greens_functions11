ee677c33b70510da4e00b3d8052918d55eeacdd6
ToruNiina <niina.toru.68u@gmail.com>
2021-03-16 12:10:32 +0900

    :white_check_mark: add comparison of drawTime of 2DAbsSym

---
+++ src/GreensFunction2DAbsSym.cpp
@@ -0,0 +1,23 @@
+#include <gf11/GreensFunction2DAbsSym.hpp>
+#include <greens_functions/GreensFunction2DAbsSym.hpp>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    namespace gf98 = greens_functions;
+
+    gf11::GreensFunction2DAbsSym gf_11(1.0, 1.0);
+    gf98::GreensFunction2DAbsSym gf_98(1.0, 1.0);
+    std::cout << "# rnd   gf11   gf98    abserr (gf11 - gf98)   relerr (|gf11/gf98 - 1|)\n";
+
+    for(std::size_t i=0; i<10000; ++i)
+    {
+        const double rnd = 0.0001 * i;
+        const double t11 = gf_11.drawTime(rnd);
+        const double t98 = gf_98.drawTime(rnd);
+        std::cout << rnd << ' ' << t11 << ' ' << t98 << ' '
+                  << t11 - t98 << ' ' << std::abs(t11 / t98 - 1.0) << '\n';
+    }
+
+    return 0;
+}
