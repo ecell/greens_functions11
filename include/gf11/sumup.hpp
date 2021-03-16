572545562798b62d4f95f9d9e63ab58b17e49709
ToruNiina <niina.toru.68u@gmail.com>
2021-03-16 12:07:00 +0900

    :recycle: fix real type as double
    the same reason as before.

--- include/gf11/sumup.hpp
+++ include/gf11/sumup.hpp
@@ -9,30 +9,32 @@
 namespace gf11
 {
 
-// F should take an unsigned integer and return a float.
-template<typename realT, typename F>
-realT sumup_until_convergence(F&& f, std::size_t max_terms, realT tol)
+// XXX: sumup_until_convergence depends on GSL, so return type is fixed as double.
+
+// F should take an unsigned integer and return a double.
+template<typename F>
+double sumup_until_convergence(F&& f, std::size_t max_terms, double tol)
 {
     using function_type = typename std::remove_const<typename std::remove_reference<F>::type>::type;
-    static_assert(std::is_same<return_type_of_t<function_type, std::uint32_t>, realT>::value, "");
+    static_assert(std::is_same<return_type_of_t<function_type, std::uint32_t>, double>::value, "");
 
     // if(relative difference < tolerance) for 4 (by default) concecutive times,
     // it's considered as converged.
     constexpr std::uint32_t convergence_check = 4;
 
-    realT sum = f(0u);
-    if(sum == realT(0))
+    double sum = f(0u);
+    if(sum == 0.0)
     {
         return 0; // this assumption is only available with the currently-used greens functions.
     }
 
-    std::vector<realT> table(max_terms);
+    std::vector<double> table(max_terms);
     table[0] = sum; // assign f(0) to table.at(0).
 
     std::uint32_t convergence_count = 0;
     for(std::size_t i=1; i<max_terms; ++i)
     {
-        const realT fi = f(i);
+        const double fi = f(i);
         table[i] = fi;
         sum += fi;
 
@@ -54,7 +56,7 @@ realT sumup_until_convergence(F&& f, std::size_t max_terms, realT tol)
     // didn't converge in [0, max_terms).
     // use series acceleration method provided by GSL.
 
-    realT error;
+    double error;
 
     std::unique_ptr<
         gsl_sum_levin_utrunc_workspace, decltype(&gsl_sum_levin_utrunc_free)
@@ -72,15 +74,14 @@ realT sumup_until_convergence(F&& f, std::size_t max_terms, realT tol)
     return sum;
 }
 
-// assuming F returns floating-point.
-template<typename realT, typename F>
-realT sumup_all(F&& f, std::size_t max_terms)
+template<typename F>
+double sumup_all(F&& f, std::size_t max_terms) noexcept
 {
     using function_type = typename std::remove_const<typename std::remove_reference<F>::type>::type;
-    static_assert(std::is_same<return_type_of_t<function_type, std::uint32_t>, realT>::value, "");
+    static_assert(std::is_same<return_type_of_t<function_type, std::uint32_t>, double>::value, "");
 
-    F_ret sum = f(0u);
-    if(sum == realT(0))
+    double sum = f(0u);
+    if(sum == 0.0)
     {
         return 0;
     }
