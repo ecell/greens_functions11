9e15eec99e33aecf26386143ce7dba1d81cb683a
ToruNiina <niina.toru.68u@gmail.com>
2021-06-06 19:49:10 +0900

    :white_check_mark: split drawTime and drawR in tests/

--- tests/GreensFunction3DAbsSym_drawR.cpp
+++ tests/GreensFunction3DAbsSym_drawR.cpp
@@ -20,42 +20,6 @@ int main(int argc, char **argv)
     gf98::GreensFunction3DAbsSym gf_98(/* D = */1.0, /* a = */1.0);
 
     // -----------------------------------------------------------------------
-    // drawTime
-
-    std::vector<double> gf98_drawTime(N);
-    const auto gf98_drawTime_start = std::chrono::system_clock::now();
-    for(std::size_t i=0; i<N; ++i)
-    {
-        const double rnd = drnd * i;
-        gf98_drawTime[i] = gf_98.drawTime(rnd);
-    }
-    const auto gf98_drawTime_dur = std::chrono::system_clock::now() - gf98_drawTime_start;
-
-    std::vector<double> gf11_drawTime(N);
-    const auto gf11_drawTime_start = std::chrono::system_clock::now();
-    for(std::size_t i=0; i<N; ++i)
-    {
-        const double rnd = drnd * i;
-        gf11_drawTime[i] = gf_11.drawTime(rnd);
-    }
-    const auto gf11_drawTime_dur = std::chrono::system_clock::now() - gf11_drawTime_start;
-
-    std::cerr << "# drawTime x" << N << "\n";
-    std::cerr << "gf98: " << format_as_seconds(gf98_drawTime_dur) << " [sec]\n";
-    std::cerr << "gf11: " << format_as_seconds(gf11_drawTime_dur) << " [sec]\n";
-
-    std::cout << "# drawTime\n";
-    std::cout << "# rnd   gf11   gf98    abserr (gf11 - gf98)   relerr (|gf11/gf98 - 1|)\n";
-    for(std::size_t i=0; i<N; ++i)
-    {
-        const double rnd = drnd * i;
-        const double t11 = gf11_drawTime[i];
-        const double t98 = gf98_drawTime[i];
-        std::cout << rnd << ' ' << t11 << ' ' << t98 << ' '
-                  << t11 - t98 << ' ' << std::abs(t11 / t98 - 1.0) << '\n';
-    }
-
-    // -----------------------------------------------------------------------
     // drawR
 
     const auto t_example = gf_11.drawTime(0.5);
